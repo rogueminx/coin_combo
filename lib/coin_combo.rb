b6cc5a563ba4295bc2be3d403ca992386fff92f0
Anna Marie Cooper and Kaila Goff <anna@>
2018-02-08 09:25:50 -0800

    pass first test to return four cents

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -1,9 +1,13 @@
 #!/usr/bin/env ruby
 
 class MakeMoneyMoney
+  # def initialize ()
+  #   @input = input
+  # end
 
-  def changemaker()
-
-
+  def changemaker(item_cost, money_paid)
+    change_array = [0.25, 0.10, 0.05, 0.01]
+    change_needed = (money_paid - item_cost)
+    change_needed
   end
 end
--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -5,6 +5,6 @@ require ('pry')
 describe ('#changemaker') do
   it ("will give 4 cents in change for something that costs a 1 cent") do
     change = MakeMoneyMoney.new()
-    expect(change.changemaker(.01, .05)).to(eq("penny:" 4))
+    expect(change.changemaker(0.01, 0.05)).to(eq(0.04))
   end
 end
