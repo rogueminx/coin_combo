83738b9180d9d964ee0ce07ccdb7ec11d2ad0882
Anna Marie Cooper and Kaila Goff <anna@>
2018-02-08 11:14:17 -0800

    wrote fifth spec

--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -15,8 +15,12 @@ describe ('#changemaker') do
     change = MakeMoneyMoney.new()
     expect(change.changemaker(8, 15)).to(eq(([5, 1, 1])))
   end
-  it ("will give 1 dime 1 nickel and 2 pennies in change for something that costs 20 cents") do
+  it ("will give 1 dime 1 nickel and 2 pennies in change for something that costs 37 cents") do
     change = MakeMoneyMoney.new()
     expect(change.changemaker(20, 37)).to(eq(([10, 5, 1, 1])))
+  end
+  it ("will give 1 quarter 1 dime 1 nickel and 2 pennies in change for something that costs 70 cents") do
+    change = MakeMoneyMoney.new()
+    expect(change.changemaker(28, 70)).to(eq(([25, 10, 5, 1, 1])))
   end
 end
