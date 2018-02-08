256c4873c64caafa1a209faae58c327eda62aa2f
Anna Marie Cooper and Kaila Goff <kaila@>
2018-02-08 10:34:51 -0800

    second spec passing

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -6,9 +6,15 @@ class MakeMoneyMoney
   # end
 
   def changemaker(item_cost, money_paid)
-    change_array = [0.25, 0.10, 0.05, 0.01]
     change_needed = (money_paid - item_cost)
-
-    change_needed
+    coin_array = []
+    until (change_needed <= 0)
+      # binding.pry
+      if (change_needed <= 0.04)
+      coin_array.push(0.01)
+      change_needed = change_needed - 0.01
+      end
+    end
+    coin_array
   end
 end
--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -3,10 +3,10 @@ require ('rspec')
 require ('pry')
 
 describe ('#changemaker') do
-  it ("will give 4 cents in change for something that costs a 1 cent") do
-    change = MakeMoneyMoney.new()
-    expect(change.changemaker(0.01, 0.05)).to(eq(0.04))
-  end
+  # it ("will give 4 cents in change for something that costs a 1 cent") do
+  #   change = MakeMoneyMoney.new()
+  #   expect(change.changemaker(0.01, 0.05)).to(eq(0.04))
+  # end
   it ("will give 4 pennies in change for something that costs a 1 cent") do
     change = MakeMoneyMoney.new()
     expect(change.changemaker(0.01, 0.05)).to(eq([0.01, 0.01, 0.01, 0.01]))
