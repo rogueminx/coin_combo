44cbeec97d8781d5b31d8a9f26da053faf8a2341
Anna Marie Cooper and Kaila Goff <anna@>
2018-02-08 11:01:59 -0800

    third spec passing

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -9,9 +9,12 @@ class MakeMoneyMoney
     change_needed = (money_paid - item_cost)
     coin_array = []
     until (change_needed <= 0)
-      if (change_needed <= 4)
-      coin_array.push(1)
-      change_needed = change_needed - 1
+      if (change_needed <= 9) & (change_needed >= 5)
+        coin_array.push(5)
+        change_needed -= 5
+      else (change_needed <= 4)
+        coin_array.push(1)
+        change_needed -= 1
       end
     end
     coin_array
